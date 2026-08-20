1c0cc9f744f1ecd99fef4cc72771669af1d27b0e
TmPixelOfficial <timblogplay.yt@gmail.com>
2026-08-20 17:09:37 +0300

    Create KristalismMod.java

---
+++ src/main/java/ru/tmpixel/kristalism/KristalismMod.java
@@ -0,0 +1,13 @@
+package ru.tmpixel.kristalism;
+
+import net.fabricmc.api.ModInitializer;
+
+public class KristalismMod implements ModInitializer {
+
+    public static final String MOD_ID = "kristalism";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Kristalism loaded!");
+    }
+}
