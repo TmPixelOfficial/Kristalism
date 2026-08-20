5152ab912b43288841d564c7540dc17ab4df06c7
TmPixelOfficial <timblogplay.yt@gmail.com>
2026-08-20 17:19:34 +0300

    Update ModItems.java

--- src/main/java/ru/tmpixel/kristalism/ModItems.java
+++ src/main/java/ru/tmpixel/kristalism/ModItems.java
@@ -5,20 +5,45 @@ import net.minecraft.item.PickaxeItem;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
 import net.minecraft.item.ToolMaterials;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static final ToolMaterial KRISTALISM_MATERIAL = new ToolMaterial(
-            ToolMaterials.DIAMOND.getInverseTag(),
-            2031,
-            30.0F,
-            5.0F,
-            25,
-            ToolMaterials.DIAMOND.getRepairIngredient()
-    );
+    public static final ToolMaterial KRISTALISM_MATERIAL = new ToolMaterial() {
+
+        @Override
+        public int getDurability() {
+            return 3000;
+        }
+
+        @Override
+        public float getMiningSpeedMultiplier() {
+            return 30.0F;
+        }
+
+        @Override
+        public float getAttackDamage() {
+            return 11.0F;
+        }
+
+        @Override
+        public int getEnchantability() {
+            return 25;
+        }
+
+        @Override
+        public Ingredient getRepairIngredient() {
+            return Ingredient.ofItems(KRISTALISM);
+        }
+
+        @Override
+        public net.minecraft.registry.tag.TagKey<net.minecraft.block.Block> getInverseTag() {
+            return ToolMaterials.DIAMOND.getInverseTag();
+        }
+    };
 
     public static final Item KRISTALISM = register(
             "kristalism",
